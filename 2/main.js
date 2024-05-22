4c7ec60567a322cf876b3603d9acfacba128aa7c
Sandro <alexanderkapanadze52@gmail.com>
2024-05-23 02:36:21 +0400

    Completed third assignment

--- 2/main.js
+++ 2/main.js
@@ -1,16 +1,16 @@
-// const users =  [{name: 'Temo', age: 25},
-// {name: 'Lasha', age: 21}, {name: 'Ana', age: 28}]
+const users =  [{name: 'Temo', age: 25},
+{name: 'Lasha', age: 21}, {name: 'Ana', age: 28}]
 
-// function findYoungest(users){
-//     ages = users.map((user) => (user.age))
-//     youngestAge = Math.min(...ages)
-//     youngestUser = users.find((user => (user.age === youngestAge)))
-//     return youngestUser.name
-// }
+function findYoungest(users){
+    ages = users.map((user) => (user.age))
+    youngestAge = Math.min(...ages)
+    youngestUser = users.find((user => (user.age === youngestAge)))
+    return youngestUser.name
+}
 
-// function cloneArray(arr) {
-//     return arr.map((item) => (item))
-// }
+function cloneArray(arr) {
+    return arr.map((item) => (item))
+}
 
-// console.log(users)
-// console.log(cloneArray(users))
+console.log(users)
+console.log(cloneArray(users))
